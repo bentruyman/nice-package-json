24df5fd5e0c2154e4dff2fca81cb8dc71d6cdbe0
Ben Truyman <ben@truyman.com>
2019-10-31 22:15:00 -0700

    chore: check for invalid flags

--- bin.js
+++ bin.js
@@ -3,14 +3,22 @@
 
 const fs = require("fs");
 const path = require("path");
-const { promisify } = require('util');
+const { promisify } = require("util");
 
-
+const c = require("ansi-colors");
 const meow = require("meow");
 
 const nicePackageJson = require("./");
 
 const writeFile = promisify(fs.writeFile);
+
+const FLAGS = {
+  write: {
+    type: "boolean"
+  }
+};
+
+const KNOWN_FLAG_NAMES = Object.keys(FLAGS);
 
 const cli = meow(
   `
@@ -20,32 +28,33 @@ const cli = meow(
   Formats package.json files consistently
 
   Options
+    --diff print a diff of any changes to be made
     --write overwrite input with fixed output
 `,
-  {
-    flags: {
-      write: {
-        type: "boolean"
-      }
-    }
-  }
+  { flags: FLAGS }
 );
 
-let pkgPath = path.join(process.cwd(), "package.json");
+Object.keys(cli.flags).forEach(flag => {
+  if (KNOWN_FLAG_NAMES.includes(flag) === false) {
+    console.log(`${c.red("Error:")} invalid argument "${flag}"`);
+    process.exit(1);
+  }
+});
 
-if (cli.input[0]) {
-  pkgPath = path.resolve(process.cwd(), cli.input[0]);
-}
+const PKG_PATH = cli.input[0] // if a package.json path is specified
+  ? path.resolve(process.cwd(), cli.input[0]) // use it
+  : path.join(process.cwd(), "package.json"); // or assume a package.json in cwd
 
-const pkg = require(pkgPath);
+// TODO: handle filenames resolve a missing file or invalid json
+const pkg = require(PKG_PATH);
 
 const formatted = nicePackageJson(pkg);
 
 async function main() {
-  if (cli.flags.write === false) {
-    return process.stdout.write(formatted);
-  } else {
+  if (cli.flags.write === true) {
     return writeFile(pkgPath, formatted);
+  } else {
+    return process.stdout.write(formatted);
   }
 }
 
