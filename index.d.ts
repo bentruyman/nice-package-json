c2b074606ad4b03d5c609719d5e8cd6cf9e287d3
Ben Truyman <ben@truyman.com>
2019-11-12 21:19:43 -0800

    chore: add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * An opinionated formatter for package.json files
+ *
+ * @param object - parsed package.json contents
+ * @returns A formatted package.json structure
+ */
+declare function nicePackageJson<T extends object>(object: T): T;
+
+export = nicePackageJson;
