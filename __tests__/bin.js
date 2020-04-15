a390b0e0bf4e2e90fe8c6e6499e2b1a19ad46d96
Ben Truyman <ben@truyman.com>
2020-04-14 18:29:51 -0700

    feat: spring cleaning
    Drop Node.js 8 support (a.k.a. stop testing against it), update
    dependencies, and simplify test names.

--- __tests__/bin.js
+++ __tests__/bin.js
@@ -18,56 +18,79 @@ function exec(options = {}, args = []) {
   });
 }
 
-describe("cli", () => {
-  describe("when no path is specified", () => {
-    it("prints a formatted package.json from the current directory", async () => {
-      const dir = tmp.dirSync();
+describe("$ nice-package-json", () => {
+  it("prints a formatted package.json from the current directory", async () => {
+    const dir = tmp.dirSync();
 
-      copyFileSync(
-        join(FIXTURES, "input/full.json"),
-        join(dir.name, "package.json")
-      );
+    copyFileSync(
+      join(FIXTURES, "input/full.json"),
+      join(dir.name, "package.json")
+    );
 
-      const { err, stdout, stderr } = await exec({ cwd: dir.name });
-      expect(err).toBeNull();
-      expect(stdout).toMatchSnapshot();
-      expect(stderr).toBe("");
-    });
-
-    it("shows an error when no package.json file is found", async () => {
-      const dir = tmp.dirSync();
-
-      const { err, stdout, stderr } = await exec({ cwd: __dirname });
-      expect(err).not.toBeNull();
-      expect(stdout).toBe("");
-      expect(stderr).toMatchSnapshot();
-    });
+    const { err, stdout, stderr } = await exec({ cwd: dir.name });
+    expect(err).toBeNull();
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toBe("");
   });
 
-  describe("when a path is specified", () => {
-    describe("and is a directory", () => {
-      it("prints a formatted package.json from the specified directory", async () => {
-        const dir = tmp.dirSync();
+  it("shows an error when no package.json file is found", async () => {
+    const dir = tmp.dirSync();
 
-        copyFileSync(
-          join(FIXTURES, "input/full.json"),
-          join(dir.name, "package.json")
-        );
-        const { err, stdout, stderr } = await exec({ cwd: __dirname }, [
-          dir.name
-        ]);
-        expect(err).toBeNull();
-        expect(stdout).toMatchSnapshot();
-        expect(stderr).toBe("");
-      });
+    const { err, stdout, stderr } = await exec({ cwd: __dirname });
+    expect(err).not.toBeNull();
+    expect(stdout).toBe("");
+    expect(stderr).toMatchSnapshot();
+  });
+});
 
-      it("shows an error when no package.json file is found", () => {});
-    });
+describe("$ nice-package-json /path/to/dir", () => {
+  it("prints a formatted package.json from the specified directory", async () => {
+    const dir = tmp.dirSync();
 
-    describe("and is a file", () => {
-      it("prints a formatted package.json using the specified file", () => {});
+    copyFileSync(
+      join(FIXTURES, "input/full.json"),
+      join(dir.name, "package.json")
+    );
+    const { err, stdout, stderr } = await exec({ cwd: __dirname }, [
+      dir.name
+    ]);
+    expect(err).toBeNull();
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toBe("");
+  });
 
-      it("shows an error if the file is not valid JSON", () => {});
-    });
+  it("shows an error when no package.json file is found", async () => {
+    const { err, stdout, stderr } = await exec({ cwd: __dirname }, [
+      "/missing/dir"
+    ]);
+    expect(err.message).toMatch("no package.json file found");
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toMatch("no package.json file found");
+  });
+});
+
+describe("$ nice-package-json /path/to/package.json", () => {
+  it("prints a formatted package.json using the specified file", async () => {
+    const dir = tmp.dirSync();
+
+    copyFileSync(
+      join(FIXTURES, "input/full.json"),
+      join(dir.name, "package.json")
+    );
+    const { err, stdout, stderr } = await exec({ cwd: dir.name }, [
+      "./package.json"
+    ]);
+    expect(err).toBeNull();
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toBe("");
+  });
+
+  it("shows an error if the file is not valid JSON", async () => {
+    const { err, stdout, stderr } = await exec({ cwd: __dirname }, [
+      "/missing/dir/package.json"
+    ]);
+    expect(err.message).toMatch("no package.json file found");
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toMatch("no package.json file found");
   });
 });
