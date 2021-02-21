79e881648dc7198de1ac942e6daee4672081e36c
Ben Truyman <ben@truyman.com>
2021-02-20 23:31:53 -0800

    refactor: externalize dependencies to deps.ts file
    And development dependencies in deps_dev.ts.

---
+++ deps_dev.ts
@@ -0,0 +1,7 @@
+export {
+  assert,
+  assertEquals,
+  assertMatch,
+  assertNotEquals,
+} from "https://deno.land/std@0.88.0/testing/asserts.ts";
+export { dirname, join } from "https://deno.land/std@0.88.0/path/mod.ts";
